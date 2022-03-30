2b46ebadcd327a50b608497da9734511b4e53e7f
metteahle <90824548+metteahle@users.noreply.github.com>
2022-03-30 09:26:30 +0200

    Add files via upload

---
+++ programmering/js/numbers.js
@@ -0,0 +1,8 @@
+//Link til tutorial: https://www.youtube.com/watch?v=DdwNHFWVzO4
+//tal der tæller op
+$(document).ready(function(){
+  $(".Mnumbers").counterUp({
+    delay: 10,
+    time: 1200
+  });
+});
